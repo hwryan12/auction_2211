4d2229442bf1f6c62c2941a341e6b1e0159ebcc1
Harrison Ryan <hwryan12@gmail.com>
2023-01-09 08:15:39 -0600

    feature: Add #add_bid test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,6 +1,7 @@
 require "rspec"
 require './lib/auction'
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Auction do
   let(:auction) { Auction.new }
@@ -28,6 +29,33 @@ RSpec.describe Auction do
       auction.add_item(item2)
       
       expect(auction.item_names).to eq (["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+
+  let(:auction) { Auction.new }
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
+  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
+  let(:item4) { Item.new('2 Days Dogsitting') }
+  let(:item5) { Item.new('Forever Stamps') }
+  let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50') }
+  let(:attendee2) { Attendee.new(name: 'Bob', budget: '$75') }
+  let(:attendee3) { Attendee.new(name: 'Mike', budget: '$100') }
+
+  describe "Iteration 2" do
+    it "#add_bid" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    
+    expect(item1.bids).to eq({})
+    
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
     end
   end
 end
