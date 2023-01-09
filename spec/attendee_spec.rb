c4cc4c21b6488ba5628a2e1f865d0ca01f84d9ac
Harrison Ryan <hwryan12@gmail.com>
2023-01-09 07:41:31 -0600

    feature: Add attendee has reabable attributes test

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe Attendee do
     it "Exists" do
       expect(attendee).to be_instance_of(Attendee)
     end
+
+    it "Has readable attributes" do
+      expect(attendee.name).to eq('Megan')
+      expect(attendee.budget).to eq('$50')
+    end
   end
 end
