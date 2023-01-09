1554b2ead6ce7493ebfe8e65e8ddc1662d6c921c
Harrison Ryan <hwryan12@gmail.com>
2023-01-09 09:19:17 -0600

    feature: Add #bidders test

--- lib/auction.rb
+++ lib/auction.rb
@@ -11,22 +11,33 @@ class Auction
 
   def item_names
     name = []
-    @items.each do |item|
-      name << item.name
-    end
+    @items.each { |item| name << item.name }
     name
   end
 
   def unpopular_items
     no_bids = []
-    @items.each do |item|
-      no_bids << item if item.bids.empty?
-    end
+    @items.each { |item| no_bids << item if item.bids.empty? }
     no_bids
   end
 
   def potential_revenue
     revenue = @items.flat_map { |item| item.current_high_bid }
     revenue.compact.sum
+  end
+
+  def bidders
+    placed_bids = []
+    @items.each do |item|
+      placed_bids << item if item.bids.any?
+    end
+    bidders = []
+    placed_bids.each do |bid|
+      bid.bids.each do |item|
+        bidders << item[0]
+      end
+    end
+    bidders.uniq
+    # require "pry";binding.pry
   end
 end
