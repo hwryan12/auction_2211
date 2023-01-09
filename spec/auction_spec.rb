862d73b85388b06e57493997704f66cb5f8110a3
Harrison Ryan <hwryan12@gmail.com>
2023-01-09 07:49:13 -0600

    feature: Add auction #add_item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,8 +1,11 @@
 require "rspec"
 require './lib/auction'
+require './lib/item'
 
 RSpec.describe Auction do
   let(:auction) { Auction.new }
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
 
   describe "#Itialize" do
     it "Exists" do
@@ -11,6 +14,13 @@ RSpec.describe Auction do
 
     it "Is created with an empty items array" do
       expect(auction.items).to eq ([])
+    end
+
+    it "Can add item to items array" do
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq ([item1, item2])
     end
   end
 end
